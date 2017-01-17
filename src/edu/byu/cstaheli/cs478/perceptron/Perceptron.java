1877a0f35d9a0a0a0c7d29644f321ff139c9a4da
Cache Staheli <cache@byu.edu>
2017-01-17 09:41:58 -0700

    Add initial files, including toolkit.

---
+++ src/edu/byu/cstaheli/cs478/perceptron/Perceptron.java
@@ -0,0 +1,29 @@
+package edu.byu.cstaheli.cs478.perceptron;
+
+import edu.byu.cstaheli.cs478.toolkit.Matrix;
+import edu.byu.cstaheli.cs478.toolkit.SupervisedLearner;
+
+import java.util.Random;
+
+/**
+ * Created by cstaheli on 1/17/2017.
+ */
+public class Perceptron extends SupervisedLearner
+{
+    public Perceptron(Random rand)
+    {
+        super();
+    }
+
+    @Override
+    public void train(Matrix features, Matrix labels) throws Exception
+    {
+
+    }
+
+    @Override
+    public void predict(double[] features, double[] labels) throws Exception
+    {
+
+    }
+}
